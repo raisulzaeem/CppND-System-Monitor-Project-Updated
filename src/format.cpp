9f141e364dd48a90879d89ad2d840f4f60f879ea
raisulzaeem <raisulzaeem@gmail.com>
2022-07-14 18:33:12 +0200

    declare stream

--- src/format.cpp
+++ src/format.cpp
@@ -12,7 +12,8 @@ string Format::ElapsedTime(long totalseconds) {
    int seconds = totalseconds % 60;
    int minutes = (totalseconds/60) % 60;
    int hours = (totalseconds/3600) % 24;
-   std::ostringstream stream<< std::setw(2) << std::setfill('0') << hours << ":" 
+   std::ostringstream stream;
+   stream<< std::setw(2) << std::setfill('0') << hours << ":" 
      << std::setw(2) << std::setfill('0') << minutes << ":"
      << std::setw(2) << std::setfill('0') << seconds;
     return stream.str();
